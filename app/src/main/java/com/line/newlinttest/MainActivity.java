adbe80098245401641c1da70baadcdb9893f7835
chenliu <1101587382@qq.com>
2018-01-20 13:14:26 +0800

    push project

---
+++ app/src/main/java/com/line/newlinttest/MainActivity.java
@@ -0,0 +1,19 @@
+package com.line.newlinttest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "sdsdsd", Toast.LENGTH_SHORT).show();
+        Log.e("MainActivity", "MainActivity");
+
+    }
+}
